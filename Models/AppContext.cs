56b3f200b81edcce7b7a71736a6fc586e785f171
S4TURN <s4turn21@gmail.com>
2023-08-20 15:34:20 +0300

    feat: add AppContext model

---
+++ Models/AppContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nauka.test.impl.Models
+{
+    public class AppContext : DbContext
+    {
+        public string DbPath { get; }
+        public DbSet<User> Users { get; set; }
+
+        public AppContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Path.Join(Environment.GetFolderPath(folder), "NAUKA");
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            DbPath = Path.Join(path, "nauka.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={DbPath}");
+        }
+    }
+}
