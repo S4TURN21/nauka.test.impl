cf12d82706294b7bd4fe03f517a1d3c9d83e53b4
S4TURN <s4turn21@gmail.com>
2023-08-20 15:34:05 +0300

    feat: add User model

---
+++ Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nauka.test.impl.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        // Фамилия
+        public string? LastName { get; set; }
+
+        // Имя
+        public string? FirstName { get; set; }
+
+        // Отчество
+        public string? Patronymic { get; set; }
+
+        // Дата рождения
+        public DateTime? DateOfBirth { get; set; }
+
+        // Адрес проживания
+        public string? ResidentialAddress { get; set; }
+
+        // Отдел
+        public string? Department { get; set; }
+
+        // "О себе"
+        public string? Comment { get; set; }
+    }
+}
